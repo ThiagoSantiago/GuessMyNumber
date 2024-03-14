8ff703a041585f5c3d1470cdfa94ff3447ad2c81
Thiago Santiago <thiagoalexandres@gmail.com>
2024-03-13 21:03:37 -0300

    Adds the validation logic for the input field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,17 +1,32 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-  const [enteredNumber, setEnteredNumber] = useState('');
+  const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputhandler(enteredText) {
-    setEnteredNumber(enteredText)
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber("");
   }
 
   function confirmInputhandler() {
+    const choseNumber = parseInt(enteredNumber);
 
+    if (isNaN(choseNumber) || choseNumber <= 0 || choseNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
+      return;
+    }
+
+    console.log("Valid number!");
   }
 
   return (
@@ -22,12 +37,12 @@ function StartGameScreen() {
         keyboardType="number-pad"
         autoCapitalize="none"
         autoCorrect={false}
+        onChangeText={numberInputhandler}
         value={enteredNumber}
-        onChange={numberInputhandler}
       />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
           <PrimaryButton onPress={confirmInputhandler}>Confirm</PrimaryButton>
@@ -67,9 +82,9 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     flexDirection: "row",
-    paddingTop: 16
+    paddingTop: 16,
   },
   buttonContainer: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
